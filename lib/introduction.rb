e076749d659dcd3142ec46d0f32984c53c105179
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-07-28 03:44:05 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,8 @@
-def introduction("Josh")
+def introduction(name = "Josh")
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language ("Dan", "Ember.JS")
+
+def introduction_with_language (name = "Dan", language = "Ember.JS")
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
